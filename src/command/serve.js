912d8f6035082b3d36782c91fc6971694f7e988f
naoki <n.globe.us@gmail.com>
2023-04-24 16:19:57 +0900

    serve

--- src/command/serve.js
+++ src/command/serve.js
@@ -3,9 +3,11 @@ import fs from 'fs'
 import path from 'path'
 import http from 'http'
 import open from 'open'
+const __dirname = path.resolve();
 
 export const serve = (source) => {
   const port = process.env.PORT || 8080
+
   // let sourcePath = path.resolve(process.cwd(), source);
 
   // if (! fs.existsSync(sourcePath)) {
@@ -19,16 +21,17 @@ export const serve = (source) => {
       case '/':
         res.statusCode = 200
         res.setHeader('Content-Type', 'text/html; charset=UTF-8')
-        const content = fs.readFileSync(path.join('..', 'docs', 'serve.html'), 'utf-8')
+
+        const content = fs.readFileSync(path.join(__dirname, 'docs', 'serve.html'), 'utf-8')
         res.end(content)
         break;
     }
+  })
 
-    server.listen(port, () => {
-      console.log(`Your map is running on http://localhost:${port}/\n`)
-      open(`http://localhost:${port}`)
-    })
-})
+  server.listen(port, () => {
+    console.log(`Your map is running on http://localhost:${port}/\n`)
+    open(`http://localhost:${port}`)
+  })
 
   console.log('serve');
 }
